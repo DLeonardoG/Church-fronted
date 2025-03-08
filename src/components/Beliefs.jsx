71f203c48c1cf7cc70501d8535841e2a82c5ce27
Leonardo Gonzalez <darkleonardog@gmail.com>
2025-03-07 22:04:12 -0500

    feat: :zap: I've been editing carousel

--- src/components/Beliefs.jsx
+++ src/components/Beliefs.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import NavBar from './NavBar'
+import Carousel from './Carousel'
 
 const Beliefs = () => {
-
-
+    const slides = [
+        'https://lh3.googleusercontent.com/p/AF1QipNC910UDY9SN7HY4YwkkmxN7blwh4Y8i0Z71lat=w1024-k',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKaeB0PzfLXmzpKsvgiXrero_N0LFuITVPTw&s'
+    ]
   return (
     <>
    <NavBar/>
+    <Carousel slides={slides}/>
    <h2>Beliefs</h2>
    </>
   );
