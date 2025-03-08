d49813449bca06e86ae2bb68b99413b84a56f669
Leonardo Gonzalez <darkleonardog@gmail.com>
2025-03-07 22:09:22 -0500

    feat: :zap: I've been editing Beliefs

--- src/components/Beliefs.jsx
+++ src/components/Beliefs.jsx
@@ -4,14 +4,14 @@ import Carousel from './Carousel'
 
 const Beliefs = () => {
     const slides = [
-        'https://lh3.googleusercontent.com/p/AF1QipNC910UDY9SN7HY4YwkkmxN7blwh4Y8i0Z71lat=w1024-k',
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKaeB0PzfLXmzpKsvgiXrero_N0LFuITVPTw&s'
+        'https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cGFpc2FqZXxlbnwwfHwwfHx8MA%3D%3D',
+        'https://media.istockphoto.com/id/517188688/es/foto/paisaje-de-monta%C3%B1a.jpg?s=612x612&w=0&k=20&c=EnSd5sJdxih_svZHscQ5Hfzr3RSOdXO9MpdmKK4CMTs='
     ]
   return (
     <>
    <NavBar/>
     <Carousel slides={slides}/>
-   <h2>Beliefs</h2>
+   <h2>Creencias</h2>
    </>
   );
 };
