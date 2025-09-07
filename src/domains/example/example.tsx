8fa8f77b839bdbda14d4315ae6e25292b14d3246
DLeonardoG <darkleonardog@gmail.com>
2025-09-07 12:38:19 -0500

    feat: :art: added menu-navegation example

--- src/domains/example/example.tsx
+++ src/domains/example/example.tsx
@@ -1,11 +1,14 @@
 import { ModeToggle } from "@/core/components/mode-toggle"
 import { LanguageToggle } from "@/core/components/language-selector"
+import { NavigationMenuDemo } from "@/core/components/menu-navegation"
 import { useTranslation } from "react-i18next"
 
 
 function Example() {
     const { t } = useTranslation();
     return (
+        <>
+        <NavigationMenuDemo/>
         <div className="flex items-center justify-center min-h-screen">
             <h1 className="scroll-m-20 text-center text-8xl font-extrabold tracking-tight text-balance">
                 {t("greeting")}
@@ -13,6 +16,7 @@ function Example() {
             <ModeToggle />
             <LanguageToggle />
         </div>
+        </>
     )
 }
 
